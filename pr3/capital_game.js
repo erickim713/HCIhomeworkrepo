8cc7c4f9600edb0b58d59956679d0d6c7df9a21d
erickim713 <erickim713@gmail.com>
2017-05-14 00:44:16 +0900

    holy shet this takes forever

--- pr3/capital_game.js
+++ pr3/capital_game.js
@@ -1,12 +1,76 @@
 $(document).ready(function(){
     var pairs = [];
     var autolist = [];
+    var postsRef;
+    var database;
+    var counter = 0;
     //functions
+
+    function initialize_firebase(){
+        var config = {
+            apiKey: "AIzaSyDgXDaJAyqZvcMqYFFzxLCRVnCBZV79lP0",
+            databaseURL: "https://prhw-8ce7a.firebaseio.com/"
+        };
+        firebase.initializeApp(config);
+        // console.log("firebase has been initialized");
+        database = firebase.database();
+        postsRef = database.ref("entry");
+        // console.log("postsRef ready")
+    }
+
+    function insert_to_database(country, capital, answer){
+        var country_entry = country;
+        var capital_entry = capital;
+        var correct = answer;
+        //console.log(country + capital + answer);
+        postsRef.push({
+            country: country_entry,
+            capital: capital_entry,
+            answer: correct
+        })
+    }
+    //this one recalls all entries.
+    function recall_all_entries(){
+        postsRef.once('value',function(snapshot){
+            snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+                insert_to_table(childData.country, childData.capital, childData.answer);
+            });
+        });
+    }
+
+    function count_all_entries(){
+        postsRef.on('value', function(snapshot) {
+            var x = snapshot.val();
+            for (var variable in x) {
+                counter = counter + 1;
+            }
+        });
+
+    }
+    function remove_from_database(country, capital, answer){
+        postsRef.once('value', function(snapshot){
+            // console.log("1");
+            // console.log(country);
+            snapshot.forEach(function(childSnapshot){
+                // console.log("2");
+                // console.log(capital);
+                var childData = childSnapshot.val();
+                // console.log(childData);
+                // console.log(answer);
+                if(childData.country == country && childData.capital == capital && childData.answer == answer){
+                    // console.log(childSnapshot.key);
+                    postsRef.child(childSnapshot.key).remove()
+                }
+            })
+        })
+    }
+
     function autocompletelist(){
         for(var i=0; i<pairs.length;i++){
             autolist.push(pairs[i]["capital"]);
         }
-    };
+    }
     function setup(result){
         var eachline = result.split("\n");
         //eachline is an array of 172 stuff in it
@@ -16,13 +80,12 @@ $(document).ready(function(){
             var first = parsed[0];
             var second = parsed[1];
             var entry = {country:first, capital:second};
-//            console.log(entry.capital);
             pairs.push(entry);
         }
         autocompletelist();
     }
-    function trim(s){ 
-        return ( s || '' ).replace( /^\s+|\s+$/g, '' ); 
+    function trim(s){
+        return ( s || '' ).replace( /^\s+|\s+$/g, '' );
     }
     function start_game(){
         new_question();
@@ -43,14 +106,35 @@ $(document).ready(function(){
     function reset_input_box(){
         document.getElementById("pr2__answer").value = "";
     }
-    function checktheanswer(question, answer){
+    function insert_to_table(question, input_answer, correct){
+        if(correct == "correct"){
+            var correctmarkup = "<tr class='right'><td class='question_slot' id='question'>" + question + "</td><td id='input_answer'>"+input_answer+"</td><td><i class='fa fa-check' aria-hidden='true'></i><button id='delete' style='float: right;'>delete</button></td></tr>";
+            $("tbody").prepend(correctmarkup);
+            if(document.getElementById('wrong').checked) {
+                $(".wrong").show();
+                $(".right").show();
+                $("#all").prop("checked", true);
+            }
+        }
+        else{
+            var wrongmarkup = "<tr class='wrong'><td class='question_slot' id='question'>" + question + "</td><td id= 'input_answer' class='wrong_capital'>" + input_answer + "</td><td id='correct_answer'>" + correct + "<button id='delete' style='float: right;'>delete</button></td></tr>";
+            $("tbody").prepend(wrongmarkup);
+            if(document.getElementById('correct').checked) {
+                $(".wrong").show();
+                $(".right").show();
+                $("#all").prop("checked", true);
+            }
+        }
+    }
+    function checktheanswer(question, input_answer){
         var i;
-        
+
         for(i=0; i<pairs.length;i++){
             if(trim(pairs[i].country) == trim(question)){
-                if(trim(pairs[i].capital)==trim(answer)){
+                if(trim(pairs[i].capital)==trim(input_answer)){
                     //correct
-                    var correctmarkup = "<tr class='right'><td id='question'>" + question + "</td><td id='answer'>"+answer+"</td><td><i class='fa fa-check' aria-hidden='true'></i><button id='delete' style='float: right;'>delete</button></td></tr>";
+                    insert_to_database(trim(question), trim(input_answer), "correct");
+                    var correctmarkup = "<tr class='right'><td class='question_slot' id='question'>" + question + "</td><td id='input_answer'>"+input_answer+"</td><td><i class='fa fa-check' aria-hidden='true'></i><button id='delete' style='float: right;'>delete</button></td></tr>";
                     $("tbody").prepend(correctmarkup);
                     if(document.getElementById('wrong').checked) {
                         $(".wrong").show();
@@ -59,8 +143,9 @@ $(document).ready(function(){
                     }
                 }
                 else{
+                    insert_to_database(trim(question), trim(input_answer), trim(pairs[i].capital));
                     var stringcapital = trim(pairs[i].capital);
-                    var wrongmarkup = "<tr class='wrong'><td>" + question + "</td><td class='wrong_capital'>" + answer + "</td><td>" + stringcapital + "<button id='delete' style='float: right;'>delete</button></td></tr>";
+                    var wrongmarkup = "<tr class='wrong'><td class='question_slot' id='question'>" + question + "</td><td id= 'input_answer' class='wrong_capital'>" + input_answer + "</td><td id='correct_answer'>" + stringcapital + "<button id='delete' style='float: right;'>delete</button></td></tr>";
                     $("tbody").prepend(wrongmarkup);
                     if(document.getElementById('correct').checked) {
                         $(".wrong").show();
@@ -72,7 +157,12 @@ $(document).ready(function(){
         }
     }
     $('tbody').on('click', "#delete", function(){
-        $(this).closest ('tr').remove ();
+        //console.log($(this).closest('tr'));
+        var row = $(this).closest('tr');
+        var cells = row.find('td');
+        $(this).closest ('tr').remove();
+        var temp = cells[2].innerHTML.split('<')
+        remove_from_database(cells[0].innerHTML, cells[1].innerHTML, temp[0]);
     });
     $('input[type="radio"]').click(function(){
         if($(this).attr('id')=="all"){
@@ -93,13 +183,14 @@ $(document).ready(function(){
         minLength: 2,
         select: function(event,ui){
             var question = $("#pr2__question").text();
-            var answer = $("#pr2__answer").val();
+            var input_answer = $("#pr2__answer").val();
             var checklist1 = pairs;
             for(var i=0; i<pairs.length;i++){
                 if(checklist1[i].country == question){
-                    if(trim(checklist1[i].capital) == answer){
+                    if(trim(checklist1[i].capital) == input_answer){
                         //correct
-                        var correctmarkup = "<tr class='right'><td>" + question + "</td><td>"+answer+"</td><td><i class='fa fa-check' aria-hidden='true'></i><button id='delete' style='float: right;'>delete</button></td></tr>";
+                        insert_to_database(trim(question), trim(input_answer), "correct");
+                        var correctmarkup = "<tr class='right'><td class='question_slot' id='question'>" + question + "</td><td>"+input_answer+"</td><td><i class='fa fa-check' aria-hidden='true'></i><button id='delete' style='float: right;'>delete</button></td></tr>";
                         $("tbody").prepend(correctmarkup);
                         if(document.getElementById('wrong').checked) {
                             $(".wrong").show();
@@ -109,7 +200,8 @@ $(document).ready(function(){
                         }
                     }
                     else{
-                        var wrongmarkup = "<tr class='wrong'><td>" + question + "</td><td class='wrong_capital'>" + answer + "</td><td>" + checklist1[i].capital + "<button id='delete' style='float: right;'>delete</button></td></tr>";
+                        insert_to_database(trim(question), trim(input_answer), trim(pairs[i].capital));
+                        var wrongmarkup = "<tr class='wrong'><td class='question_slot' id='question'>" + question + "</td><td class='wrong_capital'>" + input_answer + "</td><td>" + checklist1[i].capital + "<button id='delete' style='float: right;'>delete</button></td></tr>";
                         $("tbody").prepend(wrongmarkup);
                         if(document.getElementById('correct').checked) {
                             $(".wrong").show();
@@ -122,18 +214,24 @@ $(document).ready(function(){
             new_question();
             reset_input_box();
             FocusOnInput();
-            return false;
         }
-    });    
+    });
     $("#pr2__submit").click(function(){
         var question = $("#pr2__question").text();
-        var answer = $("#pr2__answer").val();
-        checktheanswer(question, answer);
-//        autocompletelist();
+        var input_answer = $("#pr2__answer").val();
+        checktheanswer(question, input_answer);
         reset_input_box();
         FocusOnInput();
         new_question();
     });
+
+    document.onclick = function(event) {
+        var target = event.target || event.srcElement;
+        if(event.target.id == "question"){
+            document.getElementById("googlemap").src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBk1wjoohydgdxOmjGcld1AOUbbqM_hJhU&q="+ target.innerHTML + "&maptype=satellite";
+        }
+    };
+
     $.ajax({
         url: "https://s3.ap-northeast-2.amazonaws.com/cs374-csv/country_capital_pairs.csv",
         type: 'get',
@@ -141,7 +239,9 @@ $(document).ready(function(){
         success: function(data) {
             //sets up the pairs filling.
             setup(data);
+            initialize_firebase();
+            recall_all_entries();
             start_game();
         }
-    });    
+    });
 });
